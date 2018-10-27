e01bb52903876e6fde8c5eaee58026716d640a97
Prateek Alat <prateek.alat@gmail.com>
2018-10-28 00:46:08 +0530

    Added CameraActivity layout with working camera

--- app/src/main/java/com/dshock/medicalrecordscanner/CameraActivity.kt
+++ app/src/main/java/com/dshock/medicalrecordscanner/CameraActivity.kt
@@ -1,12 +1,40 @@
 package com.dshock.medicalrecordscanner
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_camera.*
 
 class CameraActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        camera.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        camera.onResume()
+    }
+
+    override fun onPause() {
+        camera.onPause()
+        super.onPause()
+    }
+
+    override fun onStop() {
+        camera.onStop()
+        super.onStop()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        camera.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
--- app/src/main/java/com/dshock/medicalrecordscanner/MainActivity.kt
+++ app/src/main/java/com/dshock/medicalrecordscanner/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.dshock.medicalrecordscanner
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,5 +19,23 @@ class MainActivity : AppCompatActivity() {
             adapter = RecordAdapter()
             layoutManager = LinearLayoutManager(context)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when (item?.itemId) {
+            R.id.scan -> {
+                val intent = Intent(this, CameraActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        return true
     }
 }
