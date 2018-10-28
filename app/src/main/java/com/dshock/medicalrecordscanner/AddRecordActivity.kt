d7ec579ae198ae0f22fc762807998f14a2eed7e0
Prateek Alat <prateek.alat@gmail.com>
2018-10-28 16:41:45 +0530

    Added submit button to AddRecordActivity

--- app/src/main/java/com/dshock/medicalrecordscanner/AddRecordActivity.kt
+++ app/src/main/java/com/dshock/medicalrecordscanner/AddRecordActivity.kt
@@ -2,11 +2,33 @@ package com.dshock.medicalrecordscanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class AddRecordActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_record)
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when(item?.itemId) {
+            R.id.submit -> {
+                finish()
+            }
+        }
+
+        return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.menu_add_record, menu)
+
+        return true
     }
 }
